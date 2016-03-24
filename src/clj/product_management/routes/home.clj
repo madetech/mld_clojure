e32e9836ac4cb926421f891a90e8bab47cf5e6e2
Emile <emile.swarts123@gmail.com>
2016-03-24 10:58:36 +0000

    Require some validation libraries and include validation function.

--- src/clj/product_management/routes/home.clj
+++ src/clj/product_management/routes/home.clj
@@ -2,8 +2,11 @@
   (:require [product-management.layout :as layout]
             [compojure.core :refer [defroutes GET]]
             [ring.util.http-response :as response]
+            [clojure.java.io :as io]
             [product-management.db.core :as db]
-            [clojure.java.io :as io]))
+            [bouncer.core :as b]
+            [bouncer.validators :as v]
+            [ring.util.response :refer [redirect]]))
 
 (defn product-list-page []
   (layout/render
@@ -12,3 +15,10 @@
 
 (defroutes home-routes
   (GET "/" [] (product-list-page)))
+
+(defn validate-message [params]
+  (first
+    (b/validate
+      params
+      :name v/required
+      :message [v/required [v/min-count 10]])))
