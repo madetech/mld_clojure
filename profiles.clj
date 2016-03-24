49e3e3f12de71c882be91dab54a28aecb9c10743
Chris Blackburn <christopher.blackburn@gmail.com>
2016-03-24 10:15:09 +0000

    Correct database config for Vagrant

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,2 @@
-{:profiles/dev  {:env {:database-url "jdbc:mysql://localhost:3306/product_management_dev?user=db_user_name_here&password=db_user_password_here"}}
- :profiles/test {:env {:database-url "jdbc:mysql://localhost:3306/product_management_test?user=db_user_name_here&password=db_user_password_here"}}}
+{:profiles/dev  {:env {:database-url "jdbc:mysql://localhost:3306/product_management_dev?user=root&password=root"}}
+ :profiles/test {:env {:database-url "jdbc:mysql://localhost:3306/product_management_test?user=root&password=root"}}}
