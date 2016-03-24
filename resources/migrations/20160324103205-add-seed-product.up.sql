95cbe2a5357ce21eed79191caa71be1779595687
Emile <emile.swarts123@gmail.com>
2016-03-24 10:49:06 +0000

    fix schema.

--- resources/migrations/20160324103205-add-seed-product.up.sql
+++ resources/migrations/20160324103205-add-seed-product.up.sql
@@ -1,2 +1,1 @@
-alter table products modify id INTEGER NOT NULL AUTO_INCREMENT;
-insert into products (name, slug, image_url, price, is_active) values ('Test Product', 'test-product', '', 999.99, TRUE);
+ALTER table products modify id INTEGER NOT NULL AUTO_INCREMENT;
